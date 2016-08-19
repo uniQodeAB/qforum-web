8984e4a70f5646918f7e413dc43419c6e75433e7
Carl Ripa <cj.ripa@gmail.com>
2016-08-19 23:25:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# qforum-web
+Source of the website at [qforum.se](http://qforum-web.azurewebsites.net)
---
+++ src/QForum.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QForum.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //todo: fetch from Azure Storage
+            var data = new
+            {
+                RestaurantName = "Phil´s Burger",
+                RestaurantMenuUrl = "http://www.philsburger.se/#pmeny",
+                NextDate = DateTime.UtcNow.AddDays(7)
+            };
+
+            ViewData["RestaurantName"] = data.RestaurantName;
+            ViewData["RestaurantMenuUrl"] = data.RestaurantMenuUrl;
+            ViewData["NextDateReadable"] = data.NextDate.ToString("dddden dde MMMM", new CultureInfo("sv-SE"));
+
+            return View();
+        }
+        
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
